47cd2a773f1060894f611681a75034c04f32dbbd
marcelorafael <marcelorafael23@hotmail.com>
2021-08-12 19:05:53 -0300

    feat: conection axios working

--- src/presentation/Screens/Home/Home.tsx
+++ src/presentation/Screens/Home/Home.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Title } from './Home.styles'
+import GetDatas from '../../../services/axios/requisition/GetDatas'
 
 const Home: React.FC = () => {
+  const[name, setName] = useState('')
+  useEffect(() => {
+     async function loadDatas(){
+       try {
+        const response = await GetDatas('?results=5000')
+        let teste = await response.map(item => item.phone)
+
+        const datasAdapter = async () => {
+          return[{
+            gender: response.map(item => item.gender),
+            name: response.map(item => item.name.first),
+            lastName: response.map(item => item.name.last),
+            street: response.map(item => item.location.street),
+            city: response.map(item => item.location.city),
+            phone: response.map(item => item.phone),
+          }]
+        }
+
+        let teste2 = datasAdapter()
+        setName(teste)
+
+        console.log((await teste2).map(item => item.gender))
+       } catch (error) {
+         console.log(error)
+       }
+     }
+
+     loadDatas()
+  },[])
+
   return (
     <Container>
-      <Title>Home</Title>
+      <Title>{name}</Title>
     </Container>
   )
 }
--- src/services/axios/api.tsx
+++ src/services/axios/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let api = axios.create({
-  baseURL:'https://randomuser.me/api/?results=5000'
+  baseURL:'https://randomuser.me/api/'
 })
 
 export default api;
